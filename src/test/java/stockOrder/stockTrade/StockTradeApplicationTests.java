3989ec1d5f21f1977ea141c847a083d499f15464
sun <annjin12@naver.com>
2026-04-16 14:19:56 +0900

    Add project files

---
+++ src/test/java/stockOrder/stockTrade/StockTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package stockOrder.stockTrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
