f1d0932916f98b3588a63ebaf85deb7dad0ddbe6
sun <annjin12@naver.com>
2026-05-07 15:36:15 +0900

    stock create

--- src/main/java/stockOrder/stockTrade/StockTradeApplication.java
+++ src/main/java/stockOrder/stockTrade/StockTradeApplication.java
@@ -2,13 +2,16 @@ package stockOrder.stockTrade;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class StockTradeApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(StockTradeApplication.class, args);
+
 	}
 
 }
