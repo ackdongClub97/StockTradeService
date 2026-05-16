1c99e574642ea3e36721e643d9774398180241fb
sun <annjin12@naver.com>
2026-05-16 16:36:42 +0900

    naver news open api service

---
+++ src/main/java/stockOrder/stockTrade/news/service/NewsService.java
@@ -0,0 +1,53 @@
+package stockOrder.stockTrade.news.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Service
+public class NewsService {
+
+    @Value("${naver-openapi.clientId}")
+    private String clientId;
+
+    @Value("${naver-openapi.clientSecret}")
+    private String clientSecret;
+
+    private final ObjectMapper objectMapper;
+
+    public NewsService(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+     /* naver news */
+     public Mono<List<HashMap<String, String>>> getStockDetailNews(String stockName) {
+         return WebClient.create("https://openapi.naver.com")
+                 .get()
+                 .uri(uriBuilder -> uriBuilder.path("/v1/search/news.json")
+                         .queryParam("query", stockName)
+                         .queryParam("display", 5)
+                         .queryParam("start", 1)
+                         .queryParam("sort", "date")
+                         .build()
+                 ).header("X-Naver-Client-Id", clientId)
+                 .header("X-Naver-Client-Secret", clientSecret)
+                 .retrieve()
+                 .bodyToMono(String.class)  // ← String으로 받기
+                 .map(response -> {
+                     try {
+                         JsonNode items = objectMapper.readTree(response).get("items");
+                         return objectMapper.convertValue(items,
+                                 new com.fasterxml.jackson.core.type.TypeReference<List<HashMap<String, String>>>(){});
+                     } catch (Exception e) {
+                         throw new RuntimeException(e);
+                     }
+                 });
+     }
+
+}
