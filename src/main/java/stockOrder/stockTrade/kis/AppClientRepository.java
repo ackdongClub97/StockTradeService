ba2251ef2365446f7f37e4be2586f50f59d8a9f2
sun <annjin12@naver.com>
2026-05-29 12:55:15 +0900

    파일 구조 변경

--- src/main/java/stockOrder/stockTrade/kis/AppClientRepository.java
+++ src/main/java/stockOrder/stockTrade/kis/AppClientRepository.java
@@ -1,4 +1,4 @@
-package stockOrder.stockTrade.kis.repository;
+package stockOrder.stockTrade.kis;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
